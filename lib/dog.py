84e98f3cbfeb55e951a9d3817663800a22242bf2
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-31 17:41:22 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,55 +1,31 @@
-# from models import Dog
-
-# import sqlite3
-
-# CONN = sqlite3.connect('lib/dogs.db')
-# CURSOR = CONN.cursor()
-
-# def create_table(base, engine):
-#     base.metadata.create_all(engine)
-
-# def save(session, dog):
-#     session.add(dog)
-#     session.commit()
-
-# def get_all(session):
-#     dogs = session.query(Dog).all()
-#     return dogs
-
-# def find_by_name(session, name):
-#     query =  session.query(Dog).filter_by(name=name).first()
-#     return query
-# def find_by_id(session, id):
-#      query = session.query(Dog).get(id)
-#      return query
-
-# def find_by_name_and_breed(session, name, breed):
-#     query =  session.query(Dog).filter_by(name=name, breed=breed).first()
-#     return query
-
-# def update_breed(session, dog, breed):
-#     dog.breed = breed
-#     session.add(dog)
-#     session.commit()
 from models import Dog
+
+import sqlite3
+
+CONN = sqlite3.connect('lib/dogs.db')
+CURSOR = CONN.cursor()
 
 def create_table(base, engine):
     base.metadata.create_all(engine)
+
 def save(session, dog):
     session.add(dog)
     session.commit()
 
 def get_all(session):
-    return session.query(Dog).all()
+    dogs = session.query(Dog).all()
+    return dogs
 
 def find_by_name(session, name):
-    return session.query(Dog).filter(Dog.name == name).first()
-
+    query =  session.query(Dog).filter_by(name=name).first()
+    return query
 def find_by_id(session, id):
-    return session.query(Dog).filter(Dog.id == id).first()
+     query = session.query(Dog).get(id)
+     return query
 
 def find_by_name_and_breed(session, name, breed):
-    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
+    query =  session.query(Dog).filter_by(name=name, breed=breed).first()
+    return query
 
 def update_breed(session, dog, breed):
     dog.breed = breed
